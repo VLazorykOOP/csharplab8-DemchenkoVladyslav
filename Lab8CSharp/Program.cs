c418a8faa71da4c7ec5bb8f08abdb39379fcbd96
DemchenkoVladyslav <demchenkovlad80@gmail.com>
2024-04-10 09:26:04 +0300

    task 5 completed

--- Lab8CSharp/Program.cs
+++ Lab8CSharp/Program.cs
@@ -1,9 +1,23 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace Lab8CSharp
 {
     class Program
     {
+        private static void DelDir(DirectoryInfo di)
+        {
+            foreach (var f in di.GetFiles())
+            {
+                f.Delete();
+            }
+            foreach (var d in di.GetDirectories())
+            {
+                DelDir(d);
+            }
+            di.Delete();
+        }
         public static void Main(string[] Args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -82,29 +96,132 @@ namespace Lab8CSharp
 
 
 
-            Console.WriteLine("Task 4 ");
-            int n = 0;
-            int[] nums;
+            //Console.WriteLine("Task 4 ");
+            //int n = 0;
+            //int[] nums;
 
-            string[] ns = Console.ReadLine().Split();
-            n = ns.Length;
-            nums = new int[n];
-            int i = 0;
-            foreach (string s in ns)
+            //string[] ns = Console.ReadLine().Split();
+            //n = ns.Length;
+            //nums = new int[n];
+            //int i = 0;
+            //foreach (string s in ns)
+            //{
+            //    nums[i] = int.Parse(s);
+            //    i++;
+            //}
+            //using (FileStream fs = new FileStream("task4Write.bin", FileMode.OpenOrCreate))
+            //{
+            //    using (BinaryWriter bw = new BinaryWriter(fs))
+            //    {
+            //        foreach (int num in nums)
+            //        {
+            //          bw.Write(num + " ");
+            //        }
+            //    }
+            //}
+            Console.WriteLine("Task 5 ");
+            DirectoryInfo di = new(@"D:\temp");
+            try
             {
-                nums[i] = int.Parse(s);
-                i++;
+                di = new(@"D:\temp");
             }
-            using (FileStream fs = new FileStream("task4Write.bin", FileMode.OpenOrCreate))
+            catch (DirectoryNotFoundException e)
             {
-                using (BinaryWriter bw = new BinaryWriter(fs))
+                Directory.CreateDirectory(@"D:\temp");
+            }
+            finally
+            {
+                di = new(@"D:\temp");
+            }
+            string subdir1 = "Demchenko1";
+            string subdir2 = "Demchenko2";
+            di.CreateSubdirectory(subdir1);
+            di.CreateSubdirectory(subdir2);
+            FileInfo fi1 = new(di.FullName + "\\" + subdir1 + "\\t1.txt");
+            using (StreamWriter sr = fi1.CreateText())
+            {
+                sr.WriteLine("Шевченко Степан Іванович, 2001 року народження, місце проживання - м. Суми");
+            }
+
+            FileInfo fi2 = new(di.FullName + "\\" + subdir1 + "\\t2.txt");
+            using (StreamWriter sr = fi2.CreateText())
+            {
+                sr.WriteLine("Комар Сергій Федорович, 2000 року народження, місце проживання - м. Київ");
+            }
+            FileInfo fi3 = new(di.FullName + "\\Demchenko2\\t3.txt");
+            using (StreamWriter sw = fi3.CreateText())
+            {
+                using (StreamReader sr = fi1.OpenText())
                 {
-                    foreach (int num in nums)
-                    {
-                      bw.Write(num + " ");
-                    }
+                    sw.WriteLine(sr.ReadToEnd());
+                }
+                using (StreamReader sr = fi2.OpenText())
+                {
+                    sw.WriteLine(sr.ReadToEnd());
                 }
             }
+            Console.WriteLine(fi1.Attributes);
+            Console.WriteLine(fi1.CreationTime);
+            Console.WriteLine(fi1.Exists);
+            Console.WriteLine(fi1.Extension);
+            Console.WriteLine(fi1.FullName);
+            Console.WriteLine(fi1.LastAccessTime);
+            Console.WriteLine(fi1.LastWriteTime);
+            Console.WriteLine(fi1.Name);
+
+            Console.WriteLine(fi2.Attributes);
+            Console.WriteLine(fi2.CreationTime);
+            Console.WriteLine(fi2.Exists);
+            Console.WriteLine(fi2.Extension);
+            Console.WriteLine(fi2.FullName);
+            Console.WriteLine(fi2.LastAccessTime);
+            Console.WriteLine(fi2.LastWriteTime);
+            Console.WriteLine(fi2.Name);
+
+            Console.WriteLine(fi3.Attributes);
+            Console.WriteLine(fi3.CreationTime);
+            Console.WriteLine(fi3.Exists);
+            Console.WriteLine(fi3.Extension);
+            Console.WriteLine(fi3.FullName);
+            Console.WriteLine(fi3.LastAccessTime);
+            Console.WriteLine(fi3.LastWriteTime);
+            Console.WriteLine(fi3.Name);
+
+            try
+            {
+                fi2.MoveTo(di.FullName + "\\" + subdir2 + "\\" + fi2.Name);
+            }
+            catch (Exception e)
+            {
+
+            }
+            try
+            {
+                fi1.CopyTo(di.FullName + "\\" + subdir2 + "\\" + fi1.Name);
+            }
+            catch (Exception e)
+            {
+
+            }
+            DirectoryInfo di1 = new DirectoryInfo(di.FullName + "\\" + subdir1);
+            DelDir(di1);
+            DirectoryInfo di2 = new DirectoryInfo(di.FullName + "\\" + subdir2);
+            try
+            {
+                di2.MoveTo(di2.Parent + "\\ALL");
+            }
+            catch (Exception e)
+            {
+
+            }
+            Console.WriteLine(di2.Attributes);
+            Console.WriteLine(di2.CreationTime);
+            Console.WriteLine(di2.Exists);
+            Console.WriteLine(di2.Extension);
+            Console.WriteLine(di2.FullName);
+            Console.WriteLine(di2.LastAccessTime);
+            Console.WriteLine(di2.LastWriteTime);
+            Console.WriteLine(di2.Name);
         }
     }
 }
